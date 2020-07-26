e4d34c991e3ddfbe6470bbbbf072fa07f1bd2ccb
Johnnypark99 <johnnypark@cmail.carleton.ca>
2020-07-26 02:50:38 -0400

    3rd Refactor in progress, turn mechanism and tile selected mechanism complete

--- controller.py
+++ controller.py
@@ -10,11 +10,14 @@ class Controller:
         self.view.main()
 
     def on_button_click(self, caption):
-        if(caption == 'Start'):
+        if caption == 'Start':
             self.view.initiate()
-        elif(caption == 'Confirm'):
+        elif caption == 'confirm':
             self.view.turn.set(self.model.turn_pass(self.view.turn.get(),self.view.PLAYER_NUM))
             self.view.update_racks()
+        else:
+            name = 'outer.'+str(int(caption[1])-1)+'.'+caption
+            self.view.tile_selected(self.view.main_frm.nametowidget(name))
 
 if __name__ == '__main__':
     scrabble = Controller()
--- view.py
+++ view.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from random import choices
 from string import ascii_uppercase
+import json
 
 
 class View(tk.Tk):
@@ -11,12 +12,14 @@ class View(tk.Tk):
     LETTER_SCORE = [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10]
     def __init__(self,controller):
         super().__init__()
+        self.selected = False
         self.controller = controller
         self.value_var = tk.StringVar()
         self.scores = self.PLAYER_NUM*[tk.IntVar()]
         self.letters = [choices(ascii_uppercase,k=self.LETTER_NUM) for _ in range(self.PLAYER_NUM)]
         self.turn = tk.IntVar()
         self.turn.set(1)
+        self._load_file()
         self.title('Scrabble')
         self._make_main_frame()
         self._make_labels()
@@ -26,6 +29,18 @@ class View(tk.Tk):
     def main(self):
         self.mainloop()
 
+    #Update afther the turn is passed
+    def update_racks(self):
+        for widget in self.rack_frm.winfo_children():
+            widget.destroy()
+        self._generate_text(self.rack_frm)
+
+    def tile_selected(self,tile):
+        if self.selected:
+            self.selected_tile.config(highlightbackground="black")
+        self.selected = True
+        self.selected_tile = tile
+        tile.config(highlightbackground="white")
     ##Private methods
 
     def _make_main_frame(self):
@@ -34,33 +49,52 @@ class View(tk.Tk):
 
     ##Making Tiles of Scrabble
     def _make_buttons(self):
-        outer_frm= ttk.Frame(self.main_frm)
+        outer_frm= ttk.Frame(self.main_frm,name='outer')
         outer_frm.pack()
-        frm = ttk.Frame(outer_frm)
+        frm = ttk.Frame(outer_frm,name='0')
         frm.pack()
         for i in range(self.PAD):
             for j in range(self.PAD):
-                x_axis = chr(65+j)
+                x_axis = chr(97+j)
                 y_axis = i+1
-                self._configure_button(frm,(x_axis,y_axis))
-            frm = ttk.Frame(outer_frm)
+                if j == 7 and i == 7:
+                    self._configure_button(frm,'★',(x_axis+str(y_axis)))
+                else:
+                    self._configure_button(frm,str(self.board_color[j][i]),(x_axis+str(y_axis)))
+            frm = ttk.Frame(outer_frm,name=str(y_axis))
             frm.pack()
-        self._configure_button(self.main_frm,'Confirm')
+        self._configure_button(self.main_frm,'Confirm','confirm')
 
     ##A helper function for the _make_buttons function, used to add onclick feature to the buttons
-    def _configure_button(self,frm,txt):
-        btn = tk.Button(frm, text=txt,width=6,height=1,command=(
-            lambda button=txt:self.controller.on_button_click(txt)
+    def _configure_button(self,frm,txt,name):
+        btn = tk.Button(frm, text=txt,width=6,height=1,name=name,command=(
+            lambda button=name:self.controller.on_button_click(name)
             )
         )
-
         if txt == 'Confirm':
             btn.pack(side='bottom',padx=10)
             return
-
-        btn.config(bg="green",width=1, height=1, highlightbackground="black",
-borderwidth=0,activebackground="green")
+        else:
+            self._set_tile_color(btn,txt)
         btn.pack(side='left')
+
+    def _set_tile_color(self, btn, txt):
+        switcher = {
+            '0': ('','green'),
+            '1': ('2L','light blue'),
+            '2': ('2W','pink'),
+            '3': ('3L','blue'),
+            '4': ('3W','dark red'),
+            '★': ('★', 'pink')
+        }
+        btn.config(text=switcher.get(txt)[0])
+        color = switcher.get(txt)[1]
+        if txt == '3' or txt == '4':
+            txt = "white"
+        else:
+            txt = "black"
+        btn.config(bg=color,fg=txt,activeforeground=txt,activebackground=color,width=1,highlightbackground="black",borderwidth=0)
+
 
     ##Making various labels such as the scores and the indicator of a player turn
     def _make_labels(self):
@@ -71,29 +105,34 @@ borderwidth=0,activebackground="green")
             player_label.pack(side='left',padx=50)
         turn_frm = ttk.Frame(self.main_frm)
         turn_label = tk.Label(turn_frm,text="Current Turn: ")
-        turn_label.pack(side='left')
         turn_num = tk.Label(turn_frm,textvariable=self.turn)
+        turn_label.pack(side='left')
         turn_num.pack(side='left')
         turn_frm.pack()
 
     ##Displaying the rack which shows the letters that the player is given
     def _make_racks(self):
-        rack_frm = tk.Frame(self.main_frm)
-        for i in range(len(self.letters[self.turn.get()])):
-            letter = self.letters[self.turn.get()][i]
+        self.rack_frm = tk.Frame(self.main_frm)
+        self._generate_text(self.rack_frm)
+        self.rack_frm.pack(side='bottom',pady=(20,10))
+
+    def _generate_text(self,frm):
+        for i in range(len(self.letters[self.turn.get()-1])):
+            letter = self.letters[self.turn.get()-1][i]
             sub = self.LETTER_SCORE[ord(letter)-65]
-            letter_text = tk.Text(rack_frm, height=1,width=2, borderwidth=1,relief="solid",bg='Beige',font=('TkDefaultFont',20))
+            letter_text = tk.Text(frm, height=1,width=2, borderwidth=1,relief="solid",bg='Beige',font=('TkDefaultFont',20))
             letter_text.tag_configure("subscript", offset=-4,font=("TkDefaultFont",8))
             letter_text.tag_configure("center", justify='center')
             letter_text.insert("1.0",letter,"",sub,"subscript")
             letter_text.tag_add("center", "1.0", "end")
             letter_text.configure(state="disabled")
             letter_text.pack(side='left')
-        rack_frm.pack(side='bottom',pady=(20,10))
 
+    ##Loads the saved file
+    def _load_file(self):
+        savedData = json.load(open("game_data.json","r"))
+        self.board_color = savedData["board_color"]
     #Incomplete methods
-    def update_racks(self):
-        ''
 
     #Archived methods
     #def _start_frame(self):
